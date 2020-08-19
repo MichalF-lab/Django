151c69d19259623cb1f8fe61f40dd42935567c34
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-08-20 00:35:29 +0200

    commit to 'dodawanie-bazy-danych-sitemap'

---
+++ mysite/blog/feeds.py
@@ -0,0 +1,20 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from .models import Post
+
+# Subskrypcja bloga, informacja o nowych wiadomościach
+
+class LatestPostsFeed(Feed):
+    title = 'Mój blog'
+    link = '/blog/'
+    description = 'Nowe posty na moim blogu.'
+    def items(self):
+        return Post.published.all()[:5]
+    
+
+    def item_title(self, item):
+        return item.title
+    
+
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
