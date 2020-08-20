a0bca01ef7f7c482d8c35144da97f5e1892c5358
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-08-20 14:29:20 +0200

    commit on 'sitemap-subskrybcja'

--- mysite/blog/feeds.py
+++ mysite/blog/feeds.py
@@ -5,16 +5,21 @@ from .models import Post
 # Subskrypcja bloga, informacja o nowych wiadomościach
 
 class LatestPostsFeed(Feed):
+# Podklasa Feed
     title = 'Mój blog'
     link = '/blog/'
     description = 'Nowe posty na moim blogu.'
+    
     def items(self):
         return Post.published.all()[:5]
+        # Obiekty które mają być umieszczone w wdiadomości
     
 
     def item_title(self, item):
         return item.title
+        # Pobiera tytł obiektu
     
 
     def item_description(self, item):
         return truncatewords(item.body, 30)
+        # pobiera zawrtość obiektu
