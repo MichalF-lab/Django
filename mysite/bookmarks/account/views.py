ed308cea56d5c780e18530193878a46fe642d0de
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-08-23 15:32:35 +0200

    commit on master

--- mysite/bookmarks/account/views.py
+++ mysite/bookmarks/account/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
+from django.contrib.auth.decorators import login_required
+
 def user_login(request):
     if request.method == 'POST':
     # Jeżeli urzytkownik zarząda metody POST
@@ -27,3 +29,9 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request,
+                'account/dashboard.html',
+                {'section': 'dashboard'})
