8ea182f545abf3b2bc21ee04c9f8de92a2d8cc68
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-08-26 21:48:12 +0200

    commit on master

--- mysite/bookmarks/account/urls.py
+++ mysite/bookmarks/account/urls.py
@@ -3,23 +3,23 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # poprzedni widok login view
-    # path('login/', views.user_login, name='login'),
     path('login/',
         auth_views.LoginView.as_view(),
          name='login'),
+    # poprzedni widok login view
+    # path('login/', views.user_login, name='login'),
     path('logout/',
         auth_views.LogoutView.as_view(),
         name='logout'),
     path('', views.dashboard, name='dashboard'),
-    # Adresy URL przeznaczone do obsługi zmiany hasła.
+    # URL menu głównego
     path('password_change/',
         auth_views.PasswordChangeView.as_view(),
         name='password_change'),
     path('password_change/done/',
         auth_views.PasswordChangeDoneView.as_view(),
         name='password_change_done'),
-    # Adresy URL przeznaczone do obsługi procedury zerowania hasła.
+    # Adresy URL przeznaczone do obsługi zmiany hasła.
     path('password_reset/',
         auth_views.PasswordResetView.as_view(),
         name='password_reset'),
@@ -31,4 +31,5 @@ urlpatterns = [
         name='password_reset_confirm'),path('reset/done/',
         auth_views.PasswordResetCompleteView.as_view(),
         name='password_reset_complete'),
+    # Adresy URL przeznaczone do obsługi procedury zerowania hasła.
 ]
