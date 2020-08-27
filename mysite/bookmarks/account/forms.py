1b4ead83589b33e3c57fbf09c791025f56b5871a
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-08-27 23:40:18 +0200

    commit on master

--- mysite/bookmarks/account/forms.py
+++ mysite/bookmarks/account/forms.py
@@ -15,3 +15,9 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Hasła nie są identyczne.')
+        return cd['password2']
