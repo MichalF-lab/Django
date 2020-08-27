8e541d773cf9e35ea2443e330e4a52be90919390
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-08-27 23:38:31 +0200

    commit on master

--- mysite/bookmarks/account/forms.py
+++ mysite/bookmarks/account/forms.py
@@ -11,3 +11,7 @@ class LoginForm(forms.Form):
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Hasło',widget=forms.PasswordInput)
     password2 = forms.CharField(label='Powtórz hasło',widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
