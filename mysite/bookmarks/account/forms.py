52e4a8f17bb211d272c16301df2f5d1643a9376f
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-08-27 23:37:38 +0200

    commit on master

--- mysite/bookmarks/account/forms.py
+++ mysite/bookmarks/account/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
+from django.contrib.auth.models import User
+
 class LoginForm(forms.Form):
     # przygotowanie formy logowania
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     # Widżet genuruje pole HTML
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Hasło',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Powtórz hasło',widget=forms.PasswordInput)
